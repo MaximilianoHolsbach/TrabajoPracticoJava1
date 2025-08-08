c97e0934a15f16a409f7971bfd6157a4c9d3754b
MaxiCode <andresholsbach@gmail.com>
2025-08-07 23:52:10 -0300

    Se completa ejercicio 1 y 2

---
+++ src/trabajopracticojava1/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package trabajopracticojava1;
+/**
+ * 2.	Escribir y ejecutar un programa básico en Java.
+a.	Creá una clase llamada HolaMundo.
+b.	Escribe un programa que imprima el mensaje: ¡Hola, Java!
+c.	Ejecuta el programa en NetBeans y adjunta una captura del resultado en la consola.
+
+ */
+public class HolaMundo {
+    public static void main(String[] args){
+        System.out.println("Hola, Java!");
+    }
+}
